d333ff07d70139471c2e169194cac2e347a49d78
Lydia Nyamoita <lydianyamo18@outlook.com>
2023-10-02 16:33:03 +0300

    first commit

---
+++ client/src/components/App.jsx
@@ -0,0 +1,32 @@
+import { Switch, Route } from "react-router-dom";
+import Header from "./Header";
+import Restaurant from "./Restaurant";
+import Home from "./Home";
+import RestaurantPizzaForm from "./RestaurantPizzaForm";
+import Pizza from "./Pizza";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Switch>
+          <Route exact path="/restaurant_pizzas/new">
+            <RestaurantPizzaForm />
+          </Route>
+          <Route exact path="/pizzas/:id">
+            <Pizza />
+          </Route>
+          <Route exact path="/restaurants/:id">
+            <Restaurant />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
